fc174b1fb490c58276c2ef5beadbe2920aabedaa
paultltc <paul.teiletche@epfl.ch>
2024-09-04 15:04:29 +0200

    add default n_fewshots

--- src/lighteval/community_tasks/multilingual/tasks/mqa/xcopa.py
+++ src/lighteval/community_tasks/multilingual/tasks/mqa/xcopa.py
@@ -11,11 +11,12 @@ LANGS = Literal["ar", "et", "ht", "it", "id", "qu", "sw", "zh", "ta", "th", "tr"
 
 class XCopaTask(LightevalTaskConfig):
     NAME = "x_copa"
-    LANGS = Literal["ar", "et", "ht", "it", "id", "qu", "sw", "zh", "ta", "th", "tr", "vi"]
+    LANGS = Literal["ar", "en", "et", "ht", "it", "id", "qu", "sw", "zh", "ta", "th", "tr", "vi"]
 
     def __init__(self, lang: LANGS):
-        repo = "xcopa" if lang != "ar" else "OALL/AlGhafa-Arabic-LLM-Benchmark-Translated"
-        subset = lang if lang != "ar" else "copa_ext_ar"
+        repo = "OALL/AlGhafa-Arabic-LLM-Benchmark-Translated" if lang == 'ar' \
+                else "xcopa"
+        subset = "copa_ext_ar" if lang == "ar" else "translation-zh" if lang == "en" else lang 
         #TODO: The ar also has fewshots
         super().__init__(
             name=f"xcopa-{lang}",
@@ -24,7 +25,7 @@ class XCopaTask(LightevalTaskConfig):
             hf_repo=repo,
             hf_subset=subset,
             evaluation_splits=("test",),
-            few_shots_split=None,
+            few_shots_split="validation",
             few_shots_select=None,
             generation_size=-1,
             metric=(Metrics.loglikelihood_acc, Metrics.loglikelihood_acc_norm_nospace,
--- src/lighteval/main_accelerate.py
+++ src/lighteval/main_accelerate.py
@@ -90,6 +90,7 @@ def main(args):
         tasks=lighteval_config.tasks.tasks,
         pipeline_parameters=pipeline_params,
         evaluation_tracker=evaluation_tracker,
+        langs=lighteval_config.tasks.langs,
         model_config=model_config,
     )
 
